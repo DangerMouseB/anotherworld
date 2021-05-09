1c85a0632899c93281405880df8031e98e1eead2
DangerMouseB <dangermouseb@forwarding.cc>
2021-05-09 10:31:28 +0100

    add some bayes utility fns

---
+++ coppertop/db/utils.py
@@ -0,0 +1,19 @@
+# *******************************************************************************
+#
+#    Copyright (c) 2017-2021 David Briant. All rights reserved.
+#
+# *******************************************************************************
+
+
+from coppertop import pipeable
+from coppertop.std import fvPairs, each, join
+
+@pipeable
+def formatStruct(s, name, keysFormat, valuesFormat, sep):
+    def formatKv(kv):
+        k,v = kv
+        k = k if isinstance(k, str) else format(k, keysFormat)
+        v = v if isinstance(v, str) else format(v, valuesFormat)
+        return f'{k}={v}'
+    return f'{name}({s >> fvPairs >> each >> formatKv >> join >> sep})'
+
